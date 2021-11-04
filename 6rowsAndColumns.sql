92a8590e44b117b275f09f8aecc92db1c974f7c7
Kimberlly Amaral <kimberllys.amaral@gmail.com>
2021-11-04 07:38:35 -0300

    docs: update 6rowsAndColumns

--- 6rowsAndColumns.sql
+++ 6rowsAndColumns.sql
@@ -6,16 +6,19 @@ Uma tabela também pode ser chamada de entidade.
 Modelo Entidade e Relacionamento - Quando as Tabelas/entidades se relacionam entre si. 
 
 O comando para criar uma tabela é "CREATE TABLE", em seguida, vem o nome da coluna e o tipo de dado que ela aceita. */
-CREATE TABLE clients (
+CREATE TABLE clientes (
     id INT,
     nome VARCHAR(100)
 );
 
 /* O comando "INSERT INTO" insere dentro da tabela os valores mencionados.
 Dentro do parênteses utilize a ordem das colunas como parâmetro*/
-INSERT INTO clients VALUES(1, 'João da Silva');
+INSERT INTO clientes VALUES(1, 'João da Silva');
 
 /*Comando para exibição.
 O comando "SELECT" é o comando mais utilizado em banco de dados*/
-SELECT * FROM clients;
-/*PArei na aula 6 minuto 7*/
+/* O * significa que o comando pegará todos os dados */
+SELECT * FROM clientes;
+
+/* Outra forma de inserir dados na tabela */
+INSERT INTO clientes (nome, id) VALUES ('João da Silva, 2')
